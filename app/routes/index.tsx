be9e60d6efd9744159d581a91b125698474a7c50
Joyce Wambui <jwambui@protonmail.com>
2022-02-19 09:17:44 +0300

    skeleton app works, check deploy

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Test</h1>
+      <h1>Wallets Detective</h1>
       <form method="get" action="/search">
   <label>Search <input name="term" type="text" /></label>
   <button type="submit">Search</button>
--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,9 +1,45 @@
-export default function Search() {
+import type { DataFunctionArgs } from "@remix-run/server-runtime";
+import { useLoaderData } from "remix";
+import getAccountTokens from "~/data/getAccountTokens.server";
+
+type LoaderData = Awaited<ReturnType<typeof loader>>;
+
+export let loader = async ({ request }: DataFunctionArgs) => {
+
+  let url = new URL(request.url);
+  let address = url.searchParams.get("term");
+
+  if (!address) return null;
+
+  const tokens = await getAccountTokens(address);
+
+  if (!tokens) return null;
+
+  return { address, tokens };
+};
+
+export default function Index() {
+  const loaderData = useLoaderData<LoaderData>();
+
+  if (!loaderData) {
     return (
-      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-        <h1>Test</h1>
-        <p>You have found me</p>
-      </div>
+      <main>
+        <h1 className="mb-8 text-xl font-bold">No results!</h1>
+      </main>
     );
   }
-  
+
+  return (
+    <main className="p-8">
+      <h1 className="mb-8 text-xl font-bold">{loaderData.address} tokens</h1>
+
+      <ul className="flex flex-col gap-2">
+        {loaderData.tokens.map((token) => (
+          <li key={token.contractTickerSymbol}>
+            {token.contractName} - {token.formattedBalance} tokens
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
